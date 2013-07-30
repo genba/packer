be5a26dfda361a01b45c753eb760e4741ba711dc
Kalail <kashif610@gmail.com>
2013-07-30 18:15:55 -0500

    Update tasks file

--- tasks.py
+++ tasks.py
@@ -1,19 +1,5 @@
 from invoke import run, task
 
 @task
-def clean(docs=False, bytecode=False, extra=''):
-    patterns = ['build']
-    if docs:
-        patterns += 'docs/_build'
-    if bytecode:
-        patterns += '**/*.pyc'
-    if extra:
-        patterns += extra
-    for pattern in patterns:
-        run("rm -rf %s" % pattern)
-
-@task
-def build(docs=False):
-    run("python setup.py build")
-    if docs:
-        run("sphinx-build")
+def test():
+    run('py.test tests')
